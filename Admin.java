f1123c4be4e448547f0fdb7baa86124eee9182f0
Akhil Suden <59475385+Akhil-Suden@users.noreply.github.com>
2021-05-20 18:20:39 +0530

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,13 +1,23 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 class Admin{
 public static void main(String args[])throws Exception{
 Socket s=new Socket("localhost",3333);
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());
 
+Scanner input = new Scanner(System.in);
 System.out.print("Enter User name: ");
+String name = input.next();
+dout.writeUTF(name);
+dout.flush();
 System.out.print("Enter Amount to credit: ");
+int amt = input.nextInt();
+dout.writeInt(amt);
+dout.flush();
 
-}
 
 s.close();
 }}
+
