173de9803ec772ce66aec916b24e518475e2f226
Sahil Gupta <55432865+error-hacker@users.noreply.github.com>
2021-05-21 03:08:41 +0530

    Update

--- server/Server.java
+++ server/Server.java
@@ -7,53 +7,17 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.time.Duration;
 
-/*HashMap<String,User> map=new HashMap<String,Queue>();
+/*HashMap<String,User> map=new HashMap<String,Queue>();*/
 
 //final VAL=Duration.ofSeconds(10);  //validity of coupon
 
-struct coupon{
-  int amt;
-  LocalDateTime born;    //time at which coupon was created
-}
-
-class User{
-  int bal;   //balance
-  Queue<coupon> account;
-
-  User(){
-    bal=100;
-    Queue<coupon> account=new LinkedList<coupon>();
-  }
-
-  void addcoupon(int amt1){
-    this.account.offer(coupon(amt,LocalDateTime.now()));
-    System.Out.println("Amount added succesfully.");
-  }
-
-  void usecoupon(int amt1){
-    if(this.bal<amt1){
-      System.Out.println("Insufficient balance to make the transaction.");
-    }
-    else if(this.account.peek.amt>amt1){
-       this.account.peek.amt-=amt1;
-       bal-=amt1;
-      System.Out.print("Amount deducted succesfully");
-    }
-    else{
-      int ded=this.account.account.poll.amt;
-      bal-=ded;
-      this.account.usecoupon(amt1-ded);
-      System.Out.print("Amount deducted succesfully");
-        }
-    }
-  }  */
 
 public class Server {
     public static void main(String args[]) throws Exception {
         User userA = new User();
-/*User B=new User();
-map.put("A",A);
-map.put("B",B); */
+        /*User B=new User();
+        map.put("A",A);
+        map.put("B",B); */
 
         ServerSocket ss = new ServerSocket(3333);
         Socket s = ss.accept();
