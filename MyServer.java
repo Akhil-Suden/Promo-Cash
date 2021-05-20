6df50ba179ba3211a5c4004f147adf73e4845ef7
Akhil Suden <59475385+Akhil-Suden@users.noreply.github.com>
2021-05-20 19:40:00 +0530

    Server.java
    I have commented out some portion for testing purpose

--- MyServer.java
+++ MyServer.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.time.Duration;
 
-HashMap<String,User> map=new HashMap<String,Queue>();
+/*HashMap<String,User> map=new HashMap<String,Queue>();
 
 //final VAL=Duration.ofSeconds(10);  //validity of coupon
 
@@ -20,19 +20,20 @@ class User{
   Queue<coupon> account;
 
   User(){
-    bal=0;
+    bal=100;
     Queue<coupon> account=new LinkedList<coupon>();
   }
 
   void addcoupon(int amt1){
     this.account.offer(coupon(amt,LocalDateTime.now()));
+    System.Out.println("Amount added succesfully.");
   }
 
   void usecoupon(int amt1){
     if(this.bal<amt1){
       System.Out.println("Insufficient balance to make the transaction.");
     }
-    elseif (this.account.peek.amt>amt1){
+    else if(this.account.peek.amt>amt1){
        this.account.peek.amt-=amt1;
        bal-=amt1;
       System.Out.print("Amount deducted succesfully");
@@ -42,23 +43,28 @@ class User{
       bal-=ded;
       this.account.usecoupon(amt1-ded);
       System.Out.print("Amount deducted succesfully");
-                          }
+        }
     }
-  }
+  }  */
 
-class MyServer{
+class Server{
 public static void main(String args[])throws Exception{
-User A=new User();
+/*User A=new User();
 User B=new User();
 map.put("A",A);
-map.put("B",B);
-
+map.put("B",B); */
 
 ServerSocket ss=new ServerSocket(3333);
 Socket s=ss.accept();
+DataInputStream din=new DataInputStream(s.getInputStream());
+String name=(String)din.readUTF();
+System.out.println("message= "+name);
+int amt2=(int)din.readInt();
+System.out.println("message= "+amt2);
 
-}
 
+
+//din.close();
 s.close();
 ss.close();
 }}
