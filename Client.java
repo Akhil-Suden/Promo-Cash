80cbe0406cc2b5ec5425f3b86ec40ce8dd9de05a
Akhil Suden <59475385+Akhil-Suden@users.noreply.github.com>
2021-05-20 18:20:04 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -1,17 +1,24 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
+
 
 class Client{
 public static void main(String args[])throws Exception{
 Socket s=new Socket("localhost",3333);
-
+DataOutputStream dout=new DataOutputStream(s.getOutputStream());
 
 Scanner input = new Scanner(System.in);
 System.out.print("Enter User name: ");
 String name = input.next();
+dout.writeUTF(name);
+dout.flush();
 System.out.print("Enter Amount to spend: ");
-String amt = input.next();
+int amt = input.nextInt();
+dout.writeInt(amt);
+dout.flush();
 
 
 s.close();
 }}
+
